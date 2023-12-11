2fa4f70653966c0e6c1ab8be0356216208620dc6
Jacob Fedrigon <jacobfed@umich.edu>
2023-12-11 09:55:53 -0500

    Initial Commit

---
+++ Tuner Watch App/TunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunerApp.swift
+//  Tuner Watch App
+//
+//  Created by Jacob Fedrigon on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Tuner_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tuner Watch AppUITests/Tuner_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tuner_Watch_AppUITestsLaunchTests.swift
+//  Tuner Watch AppUITests
+//
+//  Created by Jacob Fedrigon on 12/11/23.
+//
+
+import XCTest
+
+final class Tuner_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
